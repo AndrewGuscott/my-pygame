ed0adc5bbbc6c13e7b9d1d82250399a631390cbe
Andrew Guscott <guscottandrew@hotmail.ca>
2025-06-06 11:43:54 -0400

    D: seperated player into update and draw groups

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	updateable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updateable, drawable)
 	player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(pygame.Color('black'))
-		player.update(dt)
-		player.draw(screen)
+		updateable.update(dt)
+		for object in drawable:
+			object.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
