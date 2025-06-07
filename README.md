55bb8759daf8a483b0008ec2bca3ebb33ffd75e6
Andrew Guscott <guscottandrew@hotmail.ca>
2025-06-06 20:33:38 -0400

    J: added launch reminder in README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my
 Asteroids
+
+call "source venv/bin/activate" before trying to play the game
--- asteroid.py
+++ asteroid.py
@@ -6,7 +6,6 @@ from circleshape import *
 class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
 		super().__init__(x, y, radius)
-		print(f'Asteroid.py: Asteroid Spawned! with radius {self.radius}')
 
 	def draw(self, screen):
 		pygame.draw.circle(screen, pygame.Color('Red'), self.position, self.radius, 2)
